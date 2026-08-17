45adfca79f30c7a68ede85bceff2e225692b7fd9
esei-melestesfay-afk <esei-meles.tesfay@elev.solleftea.se>
2026-08-17 12:27:23 +0200

    Change shortcut to Ctrl Alt Z

--- setup-shortcut.ps1
+++ setup-shortcut.ps1
@@ -7,13 +7,13 @@ $Shortcut = $Shell.CreateShortcut($ShortcutPath)
 $Shortcut.TargetPath = "powershell.exe"
 $Shortcut.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$Project\start.ps1`""
 $Shortcut.WorkingDirectory = $Project
-$Shortcut.Hotkey = "CTRL+ALT+S"
+$Shortcut.Hotkey = "CTRL+ALT+Z"
 $Shortcut.WindowStyle = 7
 $Shortcut.Description = "Starta Store Page Builder"
 $Shortcut.Save()
 
-Write-Host "" 
+Write-Host ""
 Write-Host "Klart!" -ForegroundColor Green
 Write-Host "Genvag skapad pa skrivbordet: Store Page Builder" -ForegroundColor Green
-Write-Host "Kortkommando: CTRL + ALT + S" -ForegroundColor Cyan
-Write-Host "" 
+Write-Host "Kortkommando: CTRL + ALT + Z" -ForegroundColor Cyan
+Write-Host ""
